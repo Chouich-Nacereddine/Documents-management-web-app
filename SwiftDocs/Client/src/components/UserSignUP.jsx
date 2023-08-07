2b3fd7ad65cb097584a63d6af1bd68ad48e3f1e8
Nacereddine Chouich <nacreddinechouich0012@gmail.com>
2023-08-07 20:18:57 +0200

    9th commit done with the frontend

--- SwiftDocs/Client/src/components/UserSignUP.jsx
+++ SwiftDocs/Client/src/components/UserSignUP.jsx
@@ -1,10 +1,12 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { AiOutlineCaretLeft } from "react-icons/ai";
 import Typed from "typed.js";
+import axios from "axios";
 import "./Css/Form.css";
 
 const UserSignUP = () => {
     const el = useRef(null);
+    const [typedInstance, setTypedInstance] = useState(null);
 
     useEffect(() => {
         const typed = new Typed(el.current, {
@@ -14,10 +16,39 @@ const UserSignUP = () => {
             backDelay: 1000,
         });
 
+        setTypedInstance(typed);
+
         return () => {
             typed.destroy();
         };
     }, []);
+
+    // Post to Database
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:8000/api/signup", formData)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     return (
         <div className="sm:w-[50vw] h-[88vh] bg-[#FF6600] flex flex-col">
@@ -31,31 +62,36 @@ const UserSignUP = () => {
             </div>
 
             <div className="flex justify-center items-center">
-                <form className="form">
+                <form className="form" onSubmit={handleSubmit}>
                     <div className="form_front w-full">
                         <div className="form_details text-white ">Sign Up</div>
                         <input
                             type="text"
                             className="input"
-                            placeholder="Company"
+                            placeholder="Full name"
+                            // value={formData.name}
+                            onChange={handleChange}
                         />
                         <input
                             type="text"
                             className="input"
-                            placeholder="Username or Email"
+                            placeholder="Email"
+                            // value={formData.email}
+                            onChange={handleChange}
                         />
                         <input
                             type="password"
                             className="input"
                             placeholder="Password"
+                            // value={formData.password}
+                            onChange={handleChange}
                         />
-                        <button className="btn">Login</button>
+                        <button className="btn" type="submit">
+                            Sign up
+                        </button>
                         <span className="switch text-gray-500">
-                        Already have an account?{" "}
-                            <a
-                                href="/login"
-                                className="signup_tog text-white"
-                            >
+                            Already have an account?{" "}
+                            <a href="/login" className="signup_tog text-white">
                                 Login
                             </a>
                         </span>
