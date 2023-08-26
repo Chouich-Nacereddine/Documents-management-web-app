afa6ef52e5f5a55c0680b3f7bb67ef393bc7c9d6
Nacereddine Chouich <nacreddinechouich0012@gmail.com>
2023-08-26 17:24:27 +0200

    19th commit done with the search filter logic

--- SwiftDocs/Client/src/components/FileSearch.jsx
+++ SwiftDocs/Client/src/components/FileSearch.jsx
@@ -1,8 +1,15 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import PDF from "../assets/pdf.png";
+import editicon from "../assets/editicon.png";
+import Download from "../assets/downloadPDF.png";
+import "./Css/Card.css";
 
 const FileSearch = () => {
   const [files, setFiles] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [IsEdit, setIsEdit] = useState(false);
 
   useEffect(() => {
     const fetchFiles = async () => {
@@ -18,13 +25,14 @@ const FileSearch = () => {
   }, []);
 
   console.log("files", files);
-  const [searchValue, setSearchValue] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
+  
 
   const handleSearchChange = (event) => {
     const searchTerm = event.target.value;
     setSearchValue(searchTerm);
-
+    if (searchTerm == '') {
+      setSuggestions([])
+    }
     // Filter file names based on search term
     const filteredSuggestions = files.filter((file) =>
       file.File_Name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -74,92 +82,110 @@ const FileSearch = () => {
             >
               Search
             </button>
-            {suggestions.length > 0 && (
-              <ul className="mt-2 border border-gray-600 bg-gray-700 text-white rounded-lg absolute z-10 left-0 w-full">
-                {suggestions.map((file) => (
-                  <li
-                    key={file.id}
-                    className="p-2 cursor-pointer hover:bg-blue-600"
-                  >
-                    {file.File_Name}
-                  </li>
-                ))}
-              </ul>
-            )}
           </div>
-        </form>
-      </div>
-      <div>
-        <div className="flex justify-center p-10">
-          <h1 className="text-3xl">File Edit</h1>
-        </div>
-        <form className="px-10 h-screen">
-          <div className="grid gap-6 mb-6 md:grid-cols-2">
-            <div>
-              <label
-                for="first_name"
-                className="block mb-2 text-sm font-medium text-[#FF6600]"
-              >
-                File name
-              </label>
-              <input
-                type="text"
-                id="first_name"
-                className="text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-                placeholder="John"
-              />
-            </div>
-            <div>
-              <label
-                for="last_name"
-                className="block mb-2 text-sm font-medium text-[#FF6600]"
-              >
-                Physical location
-              </label>
-              <input
-                type="text"
-                id="last_name"
-                className="text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-                placeholder="Doe"
-              />
-            </div>
-            <div>
-              <label
-                for="company"
-                className="block mb-2 text-sm font-medium text-[#FF6600]"
-              >
-                Folder name/location
-              </label>
-              <input
-                type="text"
-                id="company"
-                className="text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-                placeholder="Flowbite"
-              />
-            </div>
-            <div>
-              <label
-                for="phone"
-                className="block mb-2 text-sm font-medium text-[#FF6600]"
-              >
-                Description
-              </label>
-              <input
-                type="tel"
-                id="phone"
-                className="text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-                placeholder="123-45-678"
-                pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-              />
-            </div>
-          </div>
+          {suggestions !== "" ? (
+            <div className="flex justify-start items-center py-6 gap-6  w-[100vw]">
+              {suggestions.map((file) => (
+                <div key={file.id} className="card">
+                  <div className="content">
+                    <div className="back">
+                      <div className="back-content">
+                        <img src={PDF} alt="PDF" className="h-16" />
+                        <strong>{file.File_Name}</strong>
+                      </div>
+                    </div>
+                    <div className="front">
+                      <div className="img">
+                        <div className="circle"></div>
+                        <div className="circle" id="right"></div>
+                        <div className="circle" id="bottom"></div>
+                      </div>
 
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Save all
-          </button>
+                      <div className="front-content">
+                        <small className="badge">{file.Description}</small>
+                        <div className="description">
+                          <div className="title">
+                            <p className="title">
+                              <strong>
+                                {file.IsValidat
+                                  ? "Validated!"
+                                  : "Not Validated!"}
+                              </strong>
+                            </p>
+                          </div>
+                          <p className="card-footer flex justify-end gap-4">
+                            <img
+                              src={editicon}
+                              alt=""
+                              className="h-6 cursor-pointer"
+                            />
+                            <img
+                              src={Download}
+                              alt=""
+                              className="h-6 cursor-pointer"
+                            />
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ):
+          (
+            <div className="flex justify-start items-center py-6 gap-6  w-[100vw]" >
+
+            {
+              files.map((item,index) => (
+                <div key={index} className="card">
+                  <div className="content">
+                    <div className="back">
+                      <div className="back-content">
+                        <img src={PDF} alt="PDF" className="h-16" />
+                        <strong>{file.File_Name}</strong>
+                      </div>
+                    </div>
+                    <div className="front">
+                      <div className="img">
+                        <div className="circle"></div>
+                        <div className="circle" id="right"></div>
+                        <div className="circle" id="bottom"></div>
+                      </div>
+
+                      <div className="front-content">
+                        <small className="badge">{file.Description}</small>
+                        <div className="description">
+                          <div className="title">
+                            <p className="title">
+                              <strong>
+                                {file.IsValidat
+                                  ? "Validated!"
+                                  : "Not Validated!"}
+                              </strong>
+                            </p>
+                          </div>
+                          <p className="card-footer flex justify-end gap-4">
+                            <img
+                              src={editicon}
+                              alt=""
+                              className="h-6 cursor-pointer"
+                            />
+                            <img
+                              src={Download}
+                              alt=""
+                              className="h-6 cursor-pointer"
+                            />
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+          )}
         </form>
       </div>
     </div>
